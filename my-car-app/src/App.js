86bc9552efd969cd3acc5339fea22669b586c1ca
Heikki <bonobo@bonobo.oppikone.fi>
2017-01-04 17:17:49 +0200

    updated syntax

--- my-car-app/src/App.js
+++ my-car-app/src/App.js
@@ -3,83 +3,43 @@ import logo from './logo0000.png';
 import './App.css';
 
 
-var DataService = {
 
-	getCarList: function() {
-		return [
-			"Audi",
-			"Mercedes-Benz",
-			"Ford",
-			"Toyota",
-			"Tesla"
-
-		];
-	}
-};
-
-var AppTitle = React.createClass({
-    render: function(){
+class AppTitle extends React.Component {
+    render(){
         return (
             <h1>This is a react Demo App for {this.props.name}</h1>
         );
     }
-});
+}
 
-
-var CarList = React.createClass({
-  
-  getInitialState: function(){
-     return {
-       initialItems: [
-         "Audi",
-         "Ferrari",
-         "Porche",
-         "Testla"
-       ],
-       items: []
-     }
-  },
-  componentWillMount: function(){
-    this.setState({items: this.state.initialItems})
-  },
-  render: function(){
+class CarList extends React.Component {
+  render() {
     return (
-      <div className="car-list">
-      	<List items={this.state.items}/>
+      <div className="shopping-list">
+        <h3>Car List for {this.props.name}</h3>
+        <ul>
+          <li>Audi</li>
+          <li>Porsche</li>
+          <li>Tesla</li>
+        </ul>
       </div>
     );
   }
-});
-
-var List = React.createClass({
-  render: function(){
-    return (
-      <ul>
-      {
-        this.props.items.map(function(item) {
-          return <li key={item}>{item}</li>
-        })
-       }
-      </ul>
-    )  
-  }
-});
+}
 
 
-class App extends Component {
+class App extends React.Component {
   render() {
     return (
       <div className="App">
         <div className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
-          <h2>A ReactJS Demo by Heikki / Oppikone </h2>
+          <h2>Welcome to React</h2>
         </div>
-        <p className="App-intro">
-		<AppTitle name="Cars" />
-        </p>
-	<p className="App-carlist">
-		<CarList/>
-	</p>
+        
+	<AppTitle name="Cars"></AppTitle>
+	<CarList name="Oppikone Demo"></CarList>
+     
       </div>
     );
   }
