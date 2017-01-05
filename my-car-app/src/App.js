39bdffa42cfc8e309920f5213d115332970091b2
Heikki <bonobo@bonobo.oppikone.fi>
2017-01-05 20:19:20 +0200

    added filter list example

--- my-car-app/src/App.js
+++ my-car-app/src/App.js
@@ -7,36 +7,71 @@ import './App.css';
 class AppTitle extends React.Component {
     render(){
         return (
-            <h1>This is a Bootstrap/ReactJS Demo App for {this.props.name}</h1>
+            <h3>This is a Bootstrap / ReactJS Demo App for {this.props.name}</h3>
         );
     }
 }
 
-class CarList extends React.Component {
-  render() {
-    return (
-      <div className="car-list">
-        <h3>Car List for {this.props.name}</h3>
-        <ul class="list-group">
-          <li >Audi</li>
-          <li>Porsche</li>
-          <li>Tesla</li>
-        </ul>
-      </div>
-    );
-  }
+
+class CarFilterList extends React.Component {
+	
+	constructor(props) {
+		super(props);
+		this.handleChange = this.handleChange.bind(this);
+		this.state = {items: this.getAllItems(), initialItems: this.getAllItems()};
+	}
+	getAllItems() {
+		return  [
+				"Audi",
+				"Porsche",
+				"Tesla",
+				"Bugatti",
+				"Ferrari",
+				"Lamborghini"
+			];
+	}
+
+	handleChange(event) {
+		var searchPhrase = event.target.value;
+		var items = this.state.initialItems;
+		var filteredItems = [];
+		for (var i = 0; i < items.length; i++) {
+			var item = items[i];
+			if (item.toLowerCase().indexOf(searchPhrase.toLowerCase()) === -1) {
+				continue;
+			}
+			filteredItems.push(item);
+		}
+		this.setState({items: filteredItems});
+		
+	}
+
+	
+	render() {
+		return (
+			<div className="form-group row">
+				<p/>
+				<div className="container-fluid">
+					<input className="form-control" type="text" placeholder="Search" onChange={this.handleChange}/>
+				</div>
+				<p/>
+				<BootstrapList items={this.state.items}/>
+			</div>
+			
+		);
+ 	 }
 }
 
-class ParameterCarList extends React.Component {
+class BootstrapList extends React.Component {
 	render() {
-		var carMap = this.props.cars.map((car) =>
-			<li className="list-group-item" key={car.toString()}>
-			      {car}
+		var itemMap = this.props.items.map((item) =>
+			<li className="list-group-item" key={item.toString()}>
+			      {item}
 			</li>
   		);
 		return(
 			<div className="container-fluid">
-				<ul className="list-group">{carMap}</ul>
+				<ul className="list-group">{itemMap}</ul>
 			</div>
 		)
 	}
@@ -45,27 +80,20 @@ class ParameterCarList extends React.Component {
 
 class App extends React.Component {
 
-	getCarList() {
-		return [
-			"Audi",
-			"Porsche",
-			"Tesla"
-		];
-	}
-
 	render() {
 		return (
-			<div className="App">
-				
-				<div className="App-header">
-					<img src={logo} className="App-logo" alt="logo" />
-				  	<h2>Welcome to React</h2>
+			<div className="container-fluid">
+				<div className="App">
+					
+					<div className="App-header">
+						<img src={logo} className="App-logo" alt="logo" />
+					  	<h2>Welcome to React</h2>
+					</div>
+					
+					<AppTitle name="Cars"/>
+					
+					<CarFilterList/>
 				</div>
-				
-				<AppTitle name="Cars"></AppTitle>
-				
-				
-				<ParameterCarList cars={this.getCarList()} />
 			</div>
 		);
   	}
