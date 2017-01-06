298519b028620ec07fa847488ffffd265347b16a
Heikki <bonobo@bonobo.oppikone.fi>
2017-01-06 10:55:36 +0200

    added JSX example

---
+++ public/src/app-1.js
@@ -0,0 +1,15 @@
+
+
+var HelloOppikone = React.createClass({
+	render: function() {
+		return (
+			<h1>Hello from Oppikone!</h1>
+		)
+	}
+});
+
+ReactDOM.render(
+        <HelloOppikone/>,
+        document.getElementById('root')
+);
+
